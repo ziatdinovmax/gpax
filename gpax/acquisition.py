7844cbf9143ec6121dc6833ff8ab921f7e690e13
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2021-10-30 02:50:13 +0000

    Add module with acquisition functions

---
+++ gpax/acquisition.py
@@ -0,0 +1,104 @@
+from typing import Dict, Type
+
+import jax.numpy as jnp
+import jax.random as jra
+import numpy as onp
+import numpyro.distributions as dist
+
+from .gp import ExactGP
+
+
+def EI(rng_key: jnp.ndarray, model: Type[ExactGP],
+       X: jnp.ndarray, xi: float = 0.01,
+       maximize: bool = False, n: int = 1) -> jnp.ndarray:
+    """
+    Expected Improvement
+    """
+    y_mean, y_sampled = model.predict(rng_key, X, n=n)
+    if n > 1:
+        y_sampled = y_sampled.reshape(n * y_sampled.shape[0], -1)
+    mean, sigma = y_sampled.mean(0), y_sampled.std(0)
+    u = (mean - y_mean.max() - xi) / sigma
+    u = -u if not maximize else u
+    normal = dist.Normal(jnp.zeros_like(u), jnp.ones_like(u))
+    ucdf = normal.cdf(u)
+    updf = jnp.exp(normal.log_prob(u))
+    return sigma * (updf + u * ucdf)
+
+
+def UCB(rng_key: jnp.ndarray, model: Type[ExactGP],
+        X: jnp.ndarray, beta: float = .25,
+        maximize: bool = False, n: int = 1) -> jnp.ndarray:
+    """
+    Upper confidence bound
+    """
+    _, y_sampled = model.predict(rng_key, X, n=n)
+    if n > 1:
+        y_sampled = y_sampled.reshape(n * y_sampled.shape[0], -1)
+    mean, var = y_sampled.mean(0), y_sampled.var(0)
+    delta = jnp.sqrt(beta * var)
+    if maximize:
+        return mean + delta
+    return mean - delta
+
+
+def UE(rng_key: jnp.ndarray,
+       model: Type[ExactGP],
+       X: jnp.ndarray, n: int = 1) -> jnp.ndarray:
+    """Uncertainty-based exploration (aka kriging)"""
+    _, y_sampled = model.predict(rng_key, X, n=n)
+    if n > 1:
+        y_sampled = y_sampled.mean(1)
+    return y_sampled.var(0)
+
+
+def Thompson(rng_key: jnp.ndarray,
+             model: Type[ExactGP],
+             posterior_samples: Dict[str, jnp.ndarray],
+             X: jnp.ndarray, n: int = 1) -> jnp.ndarray:
+    """Thompson sampling"""
+    posterior_samples = model.get_samples()
+    idx = jra.randint(rng_key, (1,), 0, len(posterior_samples["k_length"]))
+    samples = {k: v[idx] for (k, v) in posterior_samples.items()}
+    _, tsample = model.predict(rng_key, X, samples, n)
+    if n > 1:
+        tsample = tsample.mean(1)
+    return tsample.squeeze()
+
+
+def bUCB(rng_key: jnp.ndarray, model: Type[ExactGP],
+         X: jnp.ndarray, batch_size: int = 4,
+         beta: float = .25,
+         maximize: bool = False,
+         n: int = 100,
+         n_restarts: int = 20) -> jnp.ndarray:
+    """
+    Batch mode for the upper confidence bound
+    """
+    std_all, obj_all = [], []
+    for i in range(n_restarts):
+        y_sampled = obtain_samples(rng_key, model, X, batch_size, n)
+        mean, var = y_sampled.mean(1), y_sampled.var(1)
+        delta = jnp.sqrt(beta * var)
+        if maximize:
+            obj = mean + delta
+            std = obj.argmax(1).std()
+        else:
+            obj = mean - delta
+            std = obj.argmin(1).std()
+        std_all.append(std)
+        obj_all.append(obj)
+    idx = onp.argmax(std_all)
+    return obj_all[idx]
+
+
+def obtain_samples(rng_key: jnp.ndarray, model: Type[ExactGP],
+                   X: jnp.ndarray, batch_size: int = 4,
+                   n: int = 500) -> jnp.ndarray:
+    posterior_samples = model.get_samples()
+    idx = onp.arange(0, len(posterior_samples["k_length"]))
+    onp.random.shuffle(idx)
+    idx = idx[:batch_size]
+    samples = {k: v[idx] for (k, v) in posterior_samples.items()}
+    _, y_sampled = model.predict(rng_key, X, samples, n)
+    return y_sampled
