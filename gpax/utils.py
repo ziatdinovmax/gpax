6e7006502e68ff819b41ab4e7e330dff709976c9
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2021-11-01 17:38:25 +0000

    Add wrapper for jax.random.split

--- gpax/utils.py
+++ gpax/utils.py
@@ -4,3 +4,12 @@ import jax
 def enable_x64():
     """Use double (x64) precision for jax arrays"""
     jax.config.update("jax_enable_x64", True)
+
+
+def get_keys(seed: int = 0):
+    """
+    Simple wrapper for jax.random.split to get
+    rng keys for model inference and prediction
+    """
+    rng_key_1, rng_key_2 = jax.random.split(jax.random.PRNGKey(0))
+    return rng_key_1, rng_key_2
