173dbb2fd6fa42303a42cf529507c5e7a2ee2ef3
ziatdinovmax <ziatdinovmax@gmail.com>
2022-01-02 14:52:17 -0500

    Update import statements

--- gpax/__init__.py
+++ gpax/__init__.py
@@ -1,6 +1,6 @@
 from . import utils, kernels, acquisition
 from .gp import ExactGP
 from .dkl import DKL
-from .vigp import 
+from .vigp import viGP
 
 __all__ = ["utils", "kernels", "acquisition", "ExactGP", "DKL"]
