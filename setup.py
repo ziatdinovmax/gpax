9893afff35b18992bc15c176c9817770813fc174
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2023-05-11 16:51:57 -0400

    Drop python-3.8

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(os.path.join(module_dir, 'gpax/__version__.py')) as f:
 if __name__ == "__main__":
     setup(
         name='gpax',
-        python_requires='>=3.8',
+        python_requires='>=3.9',
         version=__version__,
         description='Gaussian processes in NumPyro and JAX',
         long_description=open(os.path.join(module_dir, 'README.md')).read(),
