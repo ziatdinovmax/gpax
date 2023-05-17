6532d2298c5850a2d37f25c809483f5e0e7f66a2
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2023-05-17 18:03:12 -0400

    Update tests

--- tests/test_acq.py
+++ tests/test_acq.py
@@ -39,22 +39,20 @@ def test_acq_dkl(acq):
     assert_equal(obj.squeeze().shape, (len(X_new),))
 
 
-@pytest.mark.parametrize("acq", [EI, UCB, UE])
-def test_acq_penalty(acq):
+def test_acq_penalty():
     rng_keys = get_keys()
     X = onp.random.randn(8,)
     X_new = onp.random.randn(12,)
     y = 10 * X**2
     m = ExactGP(1, 'RBF')
     m.fit(rng_keys[0], X, y, num_warmup=100, num_samples=100)
-    obj1 = acq(rng_keys[1], m, X_new)
+    obj1 = UE(rng_keys[1], m, X_new)
     last_point = onp.array(X_new[3] - 0.02)
-    obj2 = acq(rng_keys[1], m, X_new, last_point=last_point)
+    obj2 = UE(rng_keys[1], m, X_new, last_point=last_point)
     assert_(onp.count_nonzero(obj1 - obj2) > 0)
 
 
-@pytest.mark.parametrize("acq", [EI, UCB, UE])
-def test_acq_penalty_indices(acq):
+def test_acq_penalty_indices():
     rng_keys = get_keys()
     h = w = 5
     X = onp.random.randn(h*w, 16)
@@ -63,8 +61,8 @@ def test_acq_penalty_indices(acq):
     X_new = onp.random.randn(h*w, 16)
     m = viDKL(input_dim=16)
     m.fit(rng_keys[0], X, y, num_steps=50)
-    obj1 = acq(rng_keys[1], m, X_new, indices=indices, last_point=indices[7])
-    obj2 = acq(rng_keys[1], m, X_new)
+    obj1 = UE(rng_keys[1], m, X_new, indices=indices, last_point=indices[7])
+    obj2 = UE(rng_keys[1], m, X_new)
     assert_(isinstance(obj1, jnp.ndarray))
     assert_equal(obj1.squeeze().shape, (len(X_new),))
     assert_(onp.count_nonzero(obj1 - obj2) > 0)
