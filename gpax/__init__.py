9cb25e3d0248d25eb13df39c0812c53888f6a1c4
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2023-07-24 23:35:21 -0400

    Update import statements

--- gpax/__init__.py
+++ gpax/__init__.py
@@ -4,11 +4,11 @@ from .vgp import vExactGP
 from .vigp import viGP
 from .spm import sPM
 from .hypo import sample_next
-from .bnn import DKL, viDKL, iBNN, vi_iBNN
+from .bnn import DKL, viDKL, iBNN, vi_iBNN, viMTDKL
 from .multitask import MultiTaskGP
 
 from .__version__ import version as __version__
 
 __all__ = ["utils", "kernels", "acquisition", "ExactGP", "vExactGP", "DKL",
-           "viDKL", "iBNN", "vi_iBNN", "MultiTaskGP", "viGP", "sPM", "sample_next",
-           "__version__"]
+           "viDKL", "iBNN", "vi_iBNN", "MultiTaskGP", "viMTDKL", "viGP", "sPM",
+           "sample_next", "__version__"]
