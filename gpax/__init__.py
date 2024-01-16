035ab78b219b9590a832ca54e13c0aeaf84a25c3
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2024-01-16 23:30:27 +0000

    Update imports

--- gpax/__init__.py
+++ gpax/__init__.py
@@ -4,8 +4,8 @@ from . import kernels
 from . import acquisition
 from .hypo import sample_next
 from .models import (DKL, CoregGP, ExactGP, MultiTaskGP, iBNN, vExactGP,
-                     vi_iBNN, viDKL, viGP, viMTDKL, VarNoiseGP)
+                     vi_iBNN, viDKL, viGP, viMTDKL, VarNoiseGP, UIGP, MeasuredNoiseGP)
 
 __all__ = ["utils", "kernels", "mtkernels", "acquisition", "ExactGP", "vExactGP", "DKL",
            "viDKL", "iBNN", "vi_iBNN", "MultiTaskGP", "viMTDKL", "viGP", "sPM", "VarNoiseGP",
-           "CoregGP", "sample_next", "__version__"]
+           "UIGP", "MeasuredNoiseGP", "CoregGP", "sample_next", "__version__"]
