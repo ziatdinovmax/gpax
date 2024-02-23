96a9215bbcd48dcb27ab52fe45950c3671765fd2
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2024-02-22 20:16:35 -0800

    Update tests

--- tests/test_dkl.py
+++ tests/test_dkl.py
@@ -32,12 +32,12 @@ def test_get_mvn_posterior():
     rng_key = get_keys()[0]
     X, y = get_dummy_data()
     X_test, _ = get_dummy_data()
-    params = {"w1": jax.random.normal(rng_key, shape=(36, 64)),
-              "w2": jax.random.normal(rng_key, shape=(64, 32)),
-              "w3": jax.random.normal(rng_key, shape=(32, 2)),
-              "b1": jax.random.normal(rng_key, shape=(64,)),
-              "b2": jax.random.normal(rng_key, shape=(32,)),
-              "b3": jax.random.normal(rng_key, shape=(2,)),
+    params = {"w0": jax.random.normal(rng_key, shape=(36, 64)),
+              "w1": jax.random.normal(rng_key, shape=(64, 32)),
+              "w2": jax.random.normal(rng_key, shape=(32, 2)),
+              "b0": jax.random.normal(rng_key, shape=(64,)),
+              "b1": jax.random.normal(rng_key, shape=(32,)),
+              "b2": jax.random.normal(rng_key, shape=(2,)),
               "k_length": jnp.array([1.0]),
               "k_scale": jnp.array(1.0),
               "noise": jnp.array(0.1)}
@@ -48,16 +48,17 @@ def test_get_mvn_posterior():
     assert_equal(mean.shape, (X_test.shape[0],))
     assert_equal(cov.shape, (X_test.shape[0], X_test.shape[0]))
 
+
 def test_get_mvn_posterior_noiseless():
     rng_key = get_keys()[0]
     X, y = get_dummy_data()
     X_test, _ = get_dummy_data()
-    params = {"w1": jax.random.normal(rng_key, shape=(36, 64)),
-              "w2": jax.random.normal(rng_key, shape=(64, 32)),
-              "w3": jax.random.normal(rng_key, shape=(32, 2)),
-              "b1": jax.random.normal(rng_key, shape=(64,)),
-              "b2": jax.random.normal(rng_key, shape=(32,)),
-              "b3": jax.random.normal(rng_key, shape=(2,)),
+    params = {"w0": jax.random.normal(rng_key, shape=(36, 64)),
+              "w1": jax.random.normal(rng_key, shape=(64, 32)),
+              "w2": jax.random.normal(rng_key, shape=(32, 2)),
+              "b0": jax.random.normal(rng_key, shape=(64,)),
+              "b1": jax.random.normal(rng_key, shape=(32,)),
+              "b2": jax.random.normal(rng_key, shape=(2,)),
               "k_length": jnp.array([1.0]),
               "k_scale": jnp.array(1.0),
               "noise": jnp.array(0.1)}
@@ -94,12 +95,12 @@ def test_jitter_mvn_posterior():
     # X = X[None]
     # y = y[None]
     # X_test = X_test[None]
-    params = {"w1": jax.random.normal(rng_key, shape=(36, 64)),
-              "w2": jax.random.normal(rng_key, shape=(64, 32)),
-              "w3": jax.random.normal(rng_key, shape=(32, 2)),
-              "b1": jax.random.normal(rng_key, shape=(64,)),
-              "b2": jax.random.normal(rng_key, shape=(32,)),
-              "b3": jax.random.normal(rng_key, shape=(2,)),
+    params = {"w0": jax.random.normal(rng_key, shape=(36, 64)),
+              "w1": jax.random.normal(rng_key, shape=(64, 32)),
+              "w2": jax.random.normal(rng_key, shape=(32, 2)),
+              "b0": jax.random.normal(rng_key, shape=(64,)),
+              "b1": jax.random.normal(rng_key, shape=(32,)),
+              "b2": jax.random.normal(rng_key, shape=(2,)),
               "k_length": jnp.array([1.0]),
               "k_scale": jnp.array(1.0),
               "noise": jnp.array(0.1)}
