562be5f00416f6ec206c0e02641abafc9d4d6051
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2024-02-25 16:42:39 -0800

    import mlp and mlp_prior from bnn module

--- gpax/models/dkl.py
+++ gpax/models/dkl.py
@@ -17,6 +17,7 @@ import numpyro.distributions as dist
 from jax import jit
 
 from .gp import ExactGP
+from .bnn import get_mlp, get_mlp_prior
 
 
 class DKL(ExactGP):
@@ -147,47 +148,3 @@ class DKL(ExactGP):
         samples = self.get_samples(1)
         numpyro.diagnostics.print_summary(
             {k: v for (k, v) in samples.items() if k in list_of_keys})
-
-
-def sample_weights(name: str, in_channels: int, out_channels: int) -> jnp.ndarray:
-    """Sampling weights matrix"""
-    w = numpyro.sample(name=name, fn=dist.Normal(
-        loc=jnp.zeros((in_channels, out_channels)),
-        scale=jnp.ones((in_channels, out_channels))))
-    return w
-
-
-def sample_biases(name: str, channels: int) -> jnp.ndarray:
-    """Sampling bias vector"""
-    b = numpyro.sample(name=name, fn=dist.Normal(
-        loc=jnp.zeros((channels)), scale=jnp.ones((channels))))
-    return b
-
-
-def get_mlp(architecture: List[int]) -> Callable[[jnp.ndarray, Dict[str, jnp.ndarray]], jnp.ndarray]:
-    """Returns a function that represents an MLP for a given architecture."""
-    def mlp(X: jnp.ndarray, params: Dict[str, jnp.ndarray]) -> jnp.ndarray:
-        """MLP for a single MCMC sample of weights and biases, handling arbitrary number of layers."""
-        h = X
-        for i in range(len(architecture)):
-            h = jnp.tanh(jnp.matmul(h, params[f"w{i}"]) + params[f"b{i}"])
-        # No non-linearity after the last layer
-        z = jnp.matmul(h, params[f"w{len(architecture)}"]) + params[f"b{len(architecture)}"]
-        return z
-    return mlp
-
-
-def get_mlp_prior(input_dim: int, output_dim: int, architecture: List[int]) -> Callable[[], Dict[str, jnp.ndarray]]:
-    """Priors over weights and biases for a Bayesian MLP"""
-    def mlp_prior():
-        params = {}
-        in_channels = input_dim
-        for i, out_channels in enumerate(architecture):
-            params[f"w{i}"] = sample_weights(f"w{i}", in_channels, out_channels)
-            params[f"b{i}"] = sample_biases(f"b{i}", out_channels)
-            in_channels = out_channels
-        # Output layer
-        params[f"w{len(architecture)}"] = sample_weights(f"w{len(architecture)}", in_channels, output_dim)
-        params[f"b{len(architecture)}"] = sample_biases(f"b{len(architecture)}", output_dim)
-        return params
-    return mlp_prior
