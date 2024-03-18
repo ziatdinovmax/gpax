8669f1403b1f67804763cf2c0985ac4ef07b01ba
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2024-03-18 05:19:40 +0000

    Move priors out of utils

--- gpax/__init__.py
+++ gpax/__init__.py
@@ -1,4 +1,5 @@
 from .__version__ import version as __version__
+from . import priors
 from . import utils
 from . import kernels
 from . import acquisition
@@ -7,6 +8,6 @@ from .models import (DKL, CoregGP, ExactGP, MultiTaskGP, iBNN, vExactGP,
                      vi_iBNN, viDKL, viGP, sPM, viMTDKL, VarNoiseGP, UIGP,
                      MeasuredNoiseGP, viSparseGP, BNN)
 
-__all__ = ["utils", "kernels", "mtkernels", "acquisition", "ExactGP", "vExactGP", "DKL",
+__all__ = ["priors", "utils", "kernels", "mtkernels", "acquisition", "ExactGP", "vExactGP", "DKL",
            "viDKL", "iBNN", "vi_iBNN", "MultiTaskGP", "viMTDKL", "viGP", "sPM", "VarNoiseGP",
            "UIGP", "MeasuredNoiseGP", "viSparseGP", "CoregGP", "BNN", "sample_next", "__version__"]
