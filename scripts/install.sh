af60f98965b887a1836c00c275faccb7d414f798
Matthew Carbone <x94carbone@gmail.com>
2024-03-29 14:19:40 -0400

    Modify scripts in scripts directory, remove unused scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
-# Good stuff. The poor man's toml parser
-# https://github.com/pypa/pip/issues/8049
-# This is the analog of pip install -e ".[...]" since for whatever reason
-# it does not appear to work cleanly with pip
-
-install_test_requirements_only () {
-    python3 -c 'import toml; c = toml.load("pyproject.toml"); print("\n".join(c["project"]["optional-dependencies"]["test"]))' | pip install -r /dev/stdin
-}
-
-install_requirements() {
-    python3 -c 'import toml; c = toml.load("pyproject.toml"); print("\n".join(c["project"]["dependencies"]))' | pip install -r /dev/stdin
-}
-
-
 pip install toml
+
 if [ "$1" = "test" ]; then
-    install_test_requirements_only
+	python3 -c 'import toml; c = toml.load("pyproject.toml"); print("\n".join(c["project"]["optional-dependencies"]["test"]))' | pip install -r /dev/stdin
+
 else
-    install_requirements
+	python3 -c 'import toml; c = toml.load("pyproject.toml"); print("\n".join(c["project"]["dependencies"]))' | pip install -r /dev/stdin
 fi
