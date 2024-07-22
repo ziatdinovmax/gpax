7178e07c7ecf34cc0db9577a8f57ec34f4628259
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2024-07-22 23:31:11 +0000

    Add device placement utility

--- gpax/utils/utils.py
+++ gpax/utils/utils.py
@@ -30,25 +30,10 @@ def get_keys(seed: int = 0):
     return rng_key_1, rng_key_2
 
 
-def split_in_batches(X_new: Union[onp.ndarray, jnp.ndarray],
-                     batch_size: int = 100, dim: int = 0):
-    """
-    Splits array into batches along the first or second dimensions
-    """
-    if dim not in [0, 1]:
-        raise NotImplementedError("'dim' must be equal to 0 or 1")
-    num_batches = jax.numpy.floor_divide(X_new.shape[dim], batch_size)
-    X_split = []
-    for i in range(num_batches):
-        if dim == 0:
-            X_i = X_new[i*batch_size:(i+1)*batch_size]
-        else:
-            X_i = X_new[:, i*batch_size:(i+1)*batch_size]
-        X_split.append(X_i)
-    X_i = X_new[(i+1)*batch_size:] if dim == 0 else X_new[:, (i+1)*batch_size:]
-    if X_i.shape[dim] > 0:
-        X_split.append(X_i)
-    return X_split
+def split_in_batches(array: jnp.ndarray, batch_size: int = 200) -> List[jnp.ndarray]:
+    """Splits array into batches"""
+    num_batches = (array.shape[0] + batch_size - 1) // batch_size
+    return [array[i * batch_size:(i + 1) * batch_size] for i in range(num_batches)]
 
 
 def split_dict(data: Dict[str, jnp.ndarray], chunk_size: int
@@ -210,3 +195,47 @@ def initialize_inducing_points(X, ratio=0.1, method='uniform', key=None):
         raise ValueError("Method must be 'uniform', 'random', or 'kmeans'")
 
     return inducing_points
+
+
+def infer_device(device_preference: str = None):
+    """
+    Returns a JAX device based on the specified preference.
+    Defaults to the first available device if no preference is given, or if the specified
+    device type is not available.
+
+    Args:
+    - device_preference (str, optional): The preferred device type ('cpu' or 'gpu').
+
+    Returns:
+    - A JAX device.
+    """
+    if device_preference:
+        # Normalize the input to lowercase to ensure compatibility.
+        device_preference = device_preference.lower()
+        # Try to get devices of the specified type.
+        devices_of_type = jax.devices(device_preference)
+        if devices_of_type:
+            # If there are any devices of the requested type, return the first one.
+            return devices_of_type[0]
+        else:
+            print(f"No devices of type '{device_preference}' found. Falling back to the default device.")
+
+    # If no preference is specified or no devices of the specified type are found, return the default device.
+    return jax.devices()[0]
+
+
+def put_on_device(device=None, *data_items):
+    """
+    Places multiple data items on the specified device.
+
+    Args:
+        device: The target device as a string (e.g., 'cpu', 'gpu'). If None, the default device is used.
+        *data_items: Variable number of data items (such as JAX array or dictionary) to be placed on the device.
+
+    Returns:
+        A tuple of the data items placed on the specified device. The structure of each data item is preserved.
+    """
+    if device is not None:
+        device = infer_device(device)
+        return tuple(jax.device_put(item, device) for item in data_items)
+    return data_items
