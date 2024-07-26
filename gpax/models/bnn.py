2ec40fec9cfb5c1c840a1d64798507e8f2d57e56
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2024-07-26 05:07:08 +0000

    Use haiku NNs in BNN. Breaking Changes!!!
    Breaking Change 1:
    - The rng_key is no longer required in .fit() or .predict()
    - The first two required arguments in .fit() are X_train and y_train
    - The first required argument in .predict() is X_new
    
    Breaking Change 2:
    - The .predict() method now directly returns posterior variance instead od posterior samples

--- gpax/models/bnn.py
+++ gpax/models/bnn.py
@@ -1,81 +1,189 @@
-"""
-bnn.py
-=======
-
-Fully Bayesian MLPs
-
-Created by Maxim Ziatdinov (email: maxim.ziatdinov@gmail.com)
-"""
-
-from typing import Callable, Dict, Optional, List, Union, Tuple
-
+from typing import Dict, Tuple, Optional, Union, List, Type
+import jax.random as jra
 import jax.numpy as jnp
+
 import numpyro
 import numpyro.distributions as dist
+from numpyro.infer import MCMC, NUTS, init_to_median, Predictive
+from numpyro.contrib.module import random_haiku_module
 
-from .spm import sPM
+import haiku as hk
+
+from .nets import HaikuMLP
+from ..utils import put_on_device, split_dict
 
 
-class BNN(sPM):
-    """Fully Bayesian MLP"""
+class BNN:
+    """
+    A Fully Bayesian Neural Network.
+    This approach employs a probabilistic treatment of all neural network weights,
+    treating them as random variables with specified prior distributions
+    and utilizing advanced Markov Chain Monte Carlo techniques to sample directly
+    from the posterior distribution, allowing to account for all plausible weight configurations.
+    This approach enables the network to make probabilistic predictions,
+    not just single-point estimates but entire distributions of possible outcomes,
+    quantifying the inherent uncertainty.
+    """
+
     def __init__(self,
                  input_dim: int,
                  output_dim: int,
-                 noise_prior_dist: Optional[Callable[[], Dict[str, jnp.ndarray]]] = None,
-                 hidden_dim: Optional[List[int]] = None, **kwargs):
-        hidden_dim = [64, 32] if not hidden_dim else hidden_dim
-        nn = kwargs.get("nn", get_mlp(hidden_dim))
-        nn_prior = kwargs.get("nn_prior", get_mlp_prior(input_dim, output_dim, hidden_dim))
-        super(BNN, self).__init__(nn, nn_prior, None, noise_prior_dist)
+                 hidden_dim: List[int] = None,
+                 activation: str = 'tanh',
+                 noise_prior: Optional[dist.Distribution] = None,
+                 nn: Type[hk.Module] = None
+                 ) -> None:
+        if noise_prior is None:
+            noise_prior = dist.HalfNormal(1.0)
+        if nn is not None:
+            self.nn_module = hk.transform(lambda x: nn()(x))
+        else:
+            hdim = hidden_dim if hidden_dim is not None else [32, 16, 8]
+            self.nn_module = hk.transform(lambda x: HaikuMLP(hdim, output_dim, activation)(x))
+        self.data_dim = (input_dim,) if isinstance(input_dim, int) else input_dim
+        self.noise_prior = noise_prior
 
-    def _set_data(self, X: jnp.ndarray, y: Optional[jnp.ndarray] = None
-                  ) -> Union[Tuple[jnp.ndarray], jnp.ndarray]:
+    def model(self, X: jnp.ndarray, y: jnp.ndarray = None, **kwargs) -> None:
+        """BNN probabilistic model"""
+
+        net = random_haiku_module(
+                "feature_extractor", self.nn_module, input_shape=(1, *self.data_dim),
+                prior=(lambda name, shape: dist.Cauchy() if name.startswith("b") else dist.Normal()))
+
+        # Pass inputs through a NN with the sampled parameters
+        mu = numpyro.deterministic("mu", net(X))
+
+        # Sample noise
+        sig = self.sample_noise()
+
+        # Score against the observed data points
+        numpyro.sample("y", dist.Normal(mu, sig), obs=y)
+
+    def fit(self, X: jnp.ndarray, y: jnp.ndarray,
+            num_warmup: int = 2000, num_samples: int = 2000,
+            num_chains: int = 1, chain_method: str = 'sequential',
+            progress_bar: bool = True, device: str = None,
+            rng_key: Optional[jnp.array] = None,
+            ) -> None:
+        """
+        Run HMC to infer parameters of the BNN
+
+        Args:
+            X: 2D feature vector
+            y: 1D target vector
+            num_warmup: number of HMC warmup states
+            num_samples: number of HMC samples
+            num_chains: number of HMC chains
+            chain_method: 'sequential', 'parallel' or 'vectorized'
+            progress_bar: show progress bar
+            device:
+                The device (e.g. "cpu" or "gpu") perform computation on ('cpu', 'gpu'). If None, computation
+                is performed on the JAX default device.
+            rng_key: random number generator key
+        """
+        key = rng_key if rng_key is not None else jra.PRNGKey(0)
+        X, y = self.set_data(X, y)
+        X, y = put_on_device(device, X, y)
+        init_strategy = init_to_median(num_samples=10)
+        kernel = NUTS(self.model, init_strategy=init_strategy)
+        self.mcmc = MCMC(
+            kernel,
+            num_warmup=num_warmup,
+            num_samples=num_samples,
+            num_chains=num_chains,
+            chain_method=chain_method,
+            progress_bar=progress_bar,
+            jit_model_args=False
+        )
+        self.mcmc.run(key, X, y)
+
+    def get_samples(self, chain_dim: bool = False) -> Dict[str, jnp.ndarray]:
+        """Get posterior samples (after running the MCMC chains)"""
+        return self.mcmc.get_samples(group_by_chain=chain_dim)
+
+    def sample_noise(self) -> jnp.ndarray:
+        """
+        Sample observational noise variance
+        """
+        return numpyro.sample("sig", self.noise_prior)
+
+    def predict(self,
+                X_new: jnp.ndarray,
+                samples: Optional[Dict[str, jnp.ndarray]] = None,
+                device: Optional[str] = None,
+                rng_key: Optional[jnp.ndarray] = None
+                ) -> Tuple[jnp.ndarray, jnp.ndarray]:
+        """
+        Predict the mean and variance of the target values for new inputs.
+
+        Args:
+            X_new:
+                New input data for predictions.
+            samples:
+                Dictionary of posterior samples with inferred model parameters (weights and biases)
+            device:
+                The device (e.g. "cpu" or "gpu") perform computation on ('cpu', 'gpu'). If None, computation
+                is performed on the JAX default device.
+            rng_key:
+                Random number generator key for JAX operations.
+
+        Returns:
+            Tuple containing the means and samples from the posterior predictive distribution.
+        """
+        X_new = self.set_data(X_new)
+
+        if rng_key is None:
+            rng_key = jra.PRNGKey(0)
+        if samples is None:
+            samples = self.get_samples(chain_dim=False)
+        X_new, samples = put_on_device(device, X_new, samples)
+
+        predictions = self.sample_from_posterior(
+            rng_key, X_new, samples, return_sites=["mu", "y"])
+        posterior_mean = predictions["mu"].mean(0)
+        posterior_var = predictions["y"].var(0)
+        return posterior_mean, posterior_var
+
+    def sample_from_posterior(self,
+                              rng_key: jnp.ndarray,
+                              X_new: jnp.ndarray,
+                              samples: Dict[str, jnp.ndarray],
+                              return_sites: Optional[List[str]] = None,
+                              ) -> jnp.ndarray:
+   
+        predictive = Predictive(
+            self.model, samples,
+            return_sites=return_sites
+        )
+        return predictive(rng_key, X_new)
+
+    # def predict_in_batches(self, X_new: jnp.ndarray,
+    #                        batch_size: int = 100,
+    #                        n_draws: int = 1,
+    #                        device: Optional[str] = None,
+    #                        rng_key: Optional[jnp.ndarray] = None
+    #                        ) -> Tuple[jnp.ndarray, jnp.ndarray]:
+    #     """
+    #     Make prediction in batches (to avoid memory overflow)
+    #     at X_new points a trained BNN model
+    #     """
+    #     samples = self.get_samples(chain_dim=False)
+    #     mean_chunks, f_samples_chunks = [], []
+    #     for batch in split_dict(samples, batch_size):
+    #         mean_i, f_samples_i = self._vmap_predict(X_new, batch, n_draws, rng_key, device)
+    #         mean_i = jax.device_put(mean_i, jax.devices("cpu")[0])
+    #         f_samples_i = jax.device_put(f_samples_i, jax.devices("cpu")[0])
+    #         mean_chunks.append(mean_i[None])
+    #         f_samples_chunks.append(f_samples_i)
+    #     mean_chunks = jnp.concatenate(mean_chunks, axis=0)
+    #     f_samples_chunks = jnp.concatenate(f_samples_chunks)
+        
+    #     return mean_chunks.mean(0), f_samples_chunks.var(0)
+
+    def set_data(self, X: jnp.ndarray, y: Optional[jnp.ndarray] = None
+                 ) -> Union[Tuple[jnp.ndarray], jnp.ndarray]:
         X = X if X.ndim > 1 else X[:, None]
         if y is not None:
             y = y[:, None] if y.ndim < 2 else y
             return X, y
         return X
-
-
-def sample_weights(name: str, in_channels: int, out_channels: int) -> jnp.ndarray:
-    """Sampling weights matrix"""
-    w = numpyro.sample(name=name, fn=dist.Normal(
-        loc=jnp.zeros((in_channels, out_channels)),
-        scale=jnp.ones((in_channels, out_channels))))
-    return w
-
-
-def sample_biases(name: str, channels: int) -> jnp.ndarray:
-    """Sampling bias vector"""
-    b = numpyro.sample(name=name, fn=dist.Cauchy(
-        loc=jnp.zeros((channels)), scale=jnp.ones((channels))))
-    return b
-
-
-def get_mlp(architecture: List[int]) -> Callable[[jnp.ndarray, Dict[str, jnp.ndarray]], jnp.ndarray]:
-    """Returns a function that represents an MLP for a given architecture."""
-    def mlp(X: jnp.ndarray, params: Dict[str, jnp.ndarray]) -> jnp.ndarray:
-        """MLP for a single MCMC sample of weights and biases, handling arbitrary number of layers."""
-        h = X
-        for i in range(len(architecture)):
-            h = jnp.tanh(jnp.matmul(h, params[f"w{i}"]) + params[f"b{i}"])
-        # No non-linearity after the last layer
-        z = jnp.matmul(h, params[f"w{len(architecture)}"]) + params[f"b{len(architecture)}"]
-        return z
-    return mlp
-
-
-def get_mlp_prior(input_dim: int, output_dim: int, architecture: List[int]) -> Callable[[], Dict[str, jnp.ndarray]]:
-    """Priors over weights and biases for a Bayesian MLP"""
-    def mlp_prior():
-        params = {}
-        in_channels = input_dim
-        for i, out_channels in enumerate(architecture):
-            params[f"w{i}"] = sample_weights(f"w{i}", in_channels, out_channels)
-            params[f"b{i}"] = sample_biases(f"b{i}", out_channels)
-            in_channels = out_channels
-        # Output layer
-        params[f"w{len(architecture)}"] = sample_weights(f"w{len(architecture)}", in_channels, output_dim)
-        params[f"b{len(architecture)}"] = sample_biases(f"b{len(architecture)}", output_dim)
-        return params
-    return mlp_prior
--- tests/test_bnn.py
+++ tests/test_bnn.py
@@ -21,27 +21,23 @@ def get_dummy_data(feature_dim=1, target_dim=1, squeezed=False):
 
 
 def test_bnn_fit():
-    key, _ = get_keys()
     X, y = get_dummy_data()
     bnn = BNN(1, 1)
-    bnn.fit(key, X, y, num_warmup=50, num_samples=50)
+    bnn.fit(X, y, num_warmup=50, num_samples=50)
     assert bnn.mcmc is not None
 
 
 def test_bnn_custom_layers_fit():
-    key, _ = get_keys()
     X, y = get_dummy_data()
-    bnn = BNN(1, 1, hidden_dim=[32, 16, 8])
-    bnn.fit(key, X, y, num_warmup=50, num_samples=50)
+    bnn = BNN(1, 1, hidden_dim=[32, 16])
+    bnn.fit(X, y, num_warmup=5, num_samples=5)
     samples = bnn.get_samples()
-    assert_equal(samples["w0"].shape, (50, 1, 32))
-    assert_equal(samples["w1"].shape, (50, 32, 16))
-    assert_equal(samples["w2"].shape, (50, 16, 8))
-    assert_equal(samples["w3"].shape, (50, 8, 1))
-    assert_equal(samples["b0"].shape, (50, 32))
-    assert_equal(samples["b1"].shape, (50, 16))
-    assert_equal(samples["b2"].shape, (50, 8))
-    assert_equal(samples["b3"].shape, (50, 1))
+    assert_equal(samples["feature_extractor/haiku_mlp/Dense0.w"].shape, (5, 1, 32))
+    assert_equal(samples["feature_extractor/haiku_mlp/Dense1.w"].shape, (5, 32, 16))
+    assert_equal(samples["feature_extractor/haiku_mlp/Dense2.w"].shape, (5, 16, 1))
+    assert_equal(samples["feature_extractor/haiku_mlp/Dense0.b"].shape, (5, 32))
+    assert_equal(samples["feature_extractor/haiku_mlp/Dense1.b"].shape, (5, 16))
+    assert_equal(samples["feature_extractor/haiku_mlp/Dense2.b"].shape, (5, 1))
 
 
 def test_bnn_predict_with_samples():
@@ -58,41 +54,19 @@ def test_bnn_predict_with_samples():
               }
     
     bnn = BNN(1, 1)
-    f_pred, f_samples = bnn.predict(key, X_test, params)
+    f_pred, f_var = bnn.predict(X_test, params)
     assert_equal(f_pred.shape, (len(X_test), 1))
-    assert_equal(f_samples.shape, (50, len(X_test), 1))
+    assert_equal(f_pred.shape, f_var.shape)
 
-
-def test_bnn_custom_layers_predict_custom_with_samples():
-    key, _ = get_keys()
-    X_test, _ = get_dummy_data()
-   
-    params = {"w0": jax.random.normal(key, shape=(50, 1, 32)),
-              "w1": jax.random.normal(key, shape=(50, 32, 16)),
-              "w2": jax.random.normal(key, shape=(50, 16, 8)),
-              "w3": jax.random.normal(key, shape=(50, 8, 1)),
-              "b0": jax.random.normal(key, shape=(50, 32,)),
-              "b1": jax.random.normal(key, shape=(50, 16,)),
-              "b2": jax.random.normal(key, shape=(50, 8,)),
-              "b3": jax.random.normal(key, shape=(50, 1,)),
-              "noise": jax.random.normal(key, shape=(50,))
-              }
-    
-    bnn = BNN(1, 1, hidden_dim=[32, 16, 8])
-    f_pred, f_samples = bnn.predict(key, X_test, params)
-    assert_equal(f_pred.shape, (len(X_test), 1))
-    assert_equal(f_samples.shape, (50, len(X_test), 1))
-    
 
 @pytest.mark.parametrize("squeezed", [True, False])
 @pytest.mark.parametrize("target_dim", [1, 2])
 @pytest.mark.parametrize("feature_dim", [1, 2])
 def test_bnn_fit_predict(feature_dim, target_dim, squeezed):
-    key, _ = get_keys()
     X, y = get_dummy_data(feature_dim, target_dim, squeezed)
     X_test, _ = get_dummy_data(feature_dim, target_dim, squeezed)
     bnn = BNN(feature_dim, target_dim, hidden_dim=[4, 2])
-    bnn.fit(key, X, y, num_warmup=5, num_samples=5)
-    f_pred, f_samples = bnn.predict(key, X_test)
+    bnn.fit(X, y, num_warmup=5, num_samples=5)
+    f_pred, f_var = bnn.predict(X_test)
     assert_equal(f_pred.shape, (len(X_test), target_dim))
-    assert_equal(f_samples.shape, (5, len(X_test), target_dim))
+    assert_equal(f_pred.shape, f_var.shape)
