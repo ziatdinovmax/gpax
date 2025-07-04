0030fdaf610557d002267728989168513b7937ca
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2025-07-03 23:06:59 -0700

    fix versioning

--- gpax/_version.py
+++ gpax/_version.py
@@ -1,10 +1,1 @@
-"""Version file. This is overwritten during build and will contain a static
-__version__ variable."""
-
-try:
-    import dunamai as _dunamai
-
-    __version__ = _dunamai.Version.from_any_vcs().serialize()
-    del _dunamai
-except (ImportError, RuntimeError):
-    __version__ = "dev"
+__version__ = '0.1.9'
